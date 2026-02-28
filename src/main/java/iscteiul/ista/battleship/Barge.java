d68a64739ce001551ac8bf24ae84496ad95bc231
Francisco Antunes <fbass@iscte-iul.pt>
2026-02-28 20:49:02 +0000

    Update Barge class documentation with test comment
    Added a test comment to the Barge class documentation.

--- src/main/java/iscteiul/ista/battleship/Barge.java
+++ src/main/java/iscteiul/ista/battleship/Barge.java
@@ -8,7 +8,8 @@ package iscteiul.ista.battleship;
  * <p>
  * Esta classe estende a funcionalidade base de {@link Ship}, definindo as 
  * características específicas de uma barca. Uma barca é o navio mais pequeno, 
- * ocupando apenas uma única posição no tabuleiro.
+ * ocupando apenas uma única posição no tabuleiro. 
+ *testar parte 2
  * </p>
 
  */
