10e1b35baba24ed0c2755d21f8d073f7a1fafa43
brite <fbass@iscte-iul.pt>
2026-02-28 20:53:53 +0000

    Merge pull request #15 from IGE-122485/Brites

--- src/main/java/iscteiul/ista/battleship/Barge.java
+++ src/main/java/iscteiul/ista/battleship/Barge.java
@@ -9,6 +9,7 @@ package iscteiul.ista.battleship;
  * Esta classe estende a funcionalidade base de {@link Ship}, definindo as 
  * características específicas de uma barca. Uma barca é o navio mais pequeno, 
  * ocupando apenas uma única posição no tabuleiro.
+ * COMENTÁRIO para gerar conflito
  * </p>
 
  */
